8ff8d1b9cf44124309152121db2e0dc6bab196d4
Arnaud Pannatier <arnaud.pannatier@idiap.ch>
2021-07-22 14:23:55 +0200

    Major changes to design.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Montserrat", "Helvetica", "Arial", "ui-sans-serif", "sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
